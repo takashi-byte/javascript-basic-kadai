a3afb53483a1bb26bfa5b1ff97b0a36c43821bb1
takashi-byte <takashi19830115@gmail.com>
2024-11-19 16:15:07 +0900

    kadai_014をアップロード

---
+++ kadai_014/square.js
@@ -0,0 +1,11 @@
+// 数の二乗を計算する関数
+function square(num) {
+    return num * num; // numの二乗を返す
+  }
+  
+  // 引数として10を渡して関数を呼び出す
+  const result = square(10);
+  
+  // 計算結果をコンソールに表示
+  console.log(result);
+  
